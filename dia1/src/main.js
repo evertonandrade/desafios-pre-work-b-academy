a5555f830f7d12010810ac89aeaea6b49ea0701c
Everton Andrade <evertonfa7@gmail.com>
2021-08-21 22:46:12 -0300

    criação de link para ocultar/exibir a app

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -4,3 +4,10 @@ document.querySelector('[data-el="app"]').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+const link = document.querySelector('[data-el="link"')
+const app = document.querySelector('[data-el="app"]')
+
+link.addEventListener('click', (event) => {
+  event.preventDefault()
+  app.classList.toggle('hide')
+})
