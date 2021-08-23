6c1b34a4fbd49f99dc0920e19628447560a5e4d2
Everton Andrade <evertonfa7@gmail.com>
2021-08-22 21:27:32 -0300

    cria select dinâmico de cores

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -18,3 +18,46 @@ function capitalizeText(str) {
   const strFormated = arr.join(' ')
   return strFormated
 }
+
+
+const form = document.querySelector('[data-el="form"]')
+const container = document.createElement('div')
+document.body.appendChild(container)
+const select = document.createElement('select')
+select.setAttribute('multiple', '')
+
+const colors = {
+  red: '#ff0000',
+  green: '#008000',
+  blue: '#000080',
+  gray: '#808080',
+  black: '#000000'
+}
+
+Object.keys(colors).forEach(color => {
+  const option = document.createElement('option')
+  option.value = color
+  option.textContent = color.toUpperCase()
+  select.appendChild(option)
+})
+
+form.appendChild(select)
+
+select.addEventListener('change', (event) => {
+  container.innerHTML = ''
+  const selectedOptions = Array.from(event.target.selectedOptions)
+  selectedOptions.forEach(option => {
+    const div = document.createElement('div')
+    div.style.width = '100px'
+    div.style.height = '100px'
+    div.style.background = colors[option.value]
+    container.appendChild(div)
+  })
+})
+
+
+
+
+
+
+
